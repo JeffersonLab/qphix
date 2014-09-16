50cbad1c542b5f3c627400c0709090b5c8a47ec5
Diptorup Deb <diptorup@cs.unc.edu>
2014-09-16 17:51:12 -0400

    Quash warning

--- include/qphix/dslash_generated.h
+++ include/qphix/dslash_generated.h
@@ -4,7 +4,7 @@
 #if defined (QPHIX_MIC_SOURCE)
 #include "qphix/mic/dslash_mic_complete_specialization.h"
 #elif defined(QPHIX_AVX_SOURCE)
-#warning including dslash_avx_complete_specializations.h"
+#warning "including dslash_avx_complete_specializations.h"
 #include "qphix/avx/dslash_avx_complete_specialization.h"
 #endif
 
