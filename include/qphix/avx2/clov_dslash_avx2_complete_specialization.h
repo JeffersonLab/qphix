9cfd4c42af443cd8426dc73551f0ad8e47aa7b9f
m-schroeck <schroeckm@gmail.com>
2015-06-28 20:57:38 +0200

    FPTYPE half was missing in template specializations

--- include/qphix/avx2/clov_dslash_avx2_complete_specialization.h
+++ include/qphix/avx2/clov_dslash_avx2_complete_specialization.h
@@ -87,6 +87,49 @@
 #undef FPTYPE
 /* -------------------- END OF DOUBLE PRECISION ----------- */
 
+/* --------------- HALF PRECISION ------------------- */
+/* HALF PRECISION IS FUNNY. SAME VECLEN/SOALEN AS SINGLE
+ * BUT THE DATA IS KEPT AS a 16 bit type. I WILL USE
+ * SHORT HERE
+ */
+
+#define FPTYPE half
+#define VEC 8
+
+// Uncompressed
+#define COMPRESS12 false
+#define COMPRESS_SUFFIX _18
+
+#define SOA 4
+#include "qphix/avx2/clov_dslash_avx2_complete_specialization_form.h"
+#undef SOA
+
+#define SOA 8
+#include "qphix/avx2/clov_dslash_avx2_complete_specialization_form.h"
+#undef SOA
+
+#undef COMPRESS12
+#undef COMPRESS_SUFFIX
+
+/* Compressed */
+#define COMPRESS12 true
+#define COMPRESS_SUFFIX _12
+
+#define SOA 4
+#include "qphix/avx2/clov_dslash_avx2_complete_specialization_form.h"
+#undef SOA
+
+#define SOA 8
+#include "qphix/avx2/clov_dslash_avx2_complete_specialization_form.h"
+#undef SOA
+
+#undef COMPRESS12
+#undef COMPRESS_SUFFIX
+
+#undef VEC
+#undef FPTYPE
+/* -------------- END OF HALF PRECISION --------------- */
+
 #undef QUOTEME
 #undef INCLUDE_FILE
 #undef INCLUDE_FILE_VAR 
