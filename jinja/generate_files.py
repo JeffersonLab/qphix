ca826bc42c85d15bf9e0a4f37fd3cca87d3f6db0
Martin Ueding <dev@martin-ueding.de>
2017-02-09 14:59:11 +0100

    Generate a Makefile.am

--- jinja/generate_files.py
+++ jinja/generate_files.py
@@ -26,11 +26,14 @@ def main():
         loader=jinja2.FileSystemLoader('..')
     )
     complete_specialization = env.get_template('jinja/complete_specialization.h.j2')
+    makefile_am = env.get_template('jinja/Makefile.am.j2')
 
     for isa, isa_data in sorted(isas.items()):
         if not os.path.isdir(os.path.join('..', isa)):
             print('Code for ISA `{}` is not generated. Skipping.'.format(isa))
             continue
+
+        os.makedirs(isa, exist_ok=True)
 
         for kernel, kernel_pattern in kernels:
             print('Working on kernel `{}` for ISA `{}` …'.format(kernel, isa))
@@ -38,17 +41,28 @@ def main():
                 (fptype, fptype_data['veclen'], fptype_data['soalens'])
                 for fptype, fptype_data in sorted(isa_data['fptypes'].items())]
 
+            # Generate the complete specialization.
             rendered = complete_specialization.render(
                 ISA=isa,
                 kernel_base=kernel,
                 kernel_pattern=kernel_pattern,
                 defines=defines,
+                extra_includes=isa_data['extra_includes'],
             )
-
-            os.makedirs(isa, exist_ok=True)
             filename = os.path.join(isa, '{}_{}_complete_specialization.h'.format(kernel, isa))
             with open(filename, 'w') as f:
                 f.write(rendered)
+
+        # Generate a `Makefile.am`.
+        rendered = makefile_am.render(
+            isa=isa,
+            extra_includes=[
+                os.path.relpath(extra_include, os.path.join('qphix', isa))
+                for extra_include in isa_data['extra_includes']]
+        )
+        filename = os.path.join(isa, 'Makefile.am')
+        with open(filename, 'w') as f:
+            f.write(rendered)
 
 
 def _parse_args():
