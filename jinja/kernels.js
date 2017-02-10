e0585ce0df4d1fac5a46856f7b818a0102b70cf2
Martin Ueding <dev@martin-ueding.de>
2017-02-10 20:55:58 +0100

    Start duplicating the glue code

--- jinja/kernels.js
+++ jinja/kernels.js
@@ -2,7 +2,7 @@
     "clov_%(fptype_underscore)sdslash",
     "dslash",
     "tmf_clov_%(fptype_underscore)sdslash",
-    "tmf_dslash"
+    "tmf_dslash",
     "ndtm_clov_%(fptype_underscore)sdslash",
     "ndtm_dslash"
 ]
