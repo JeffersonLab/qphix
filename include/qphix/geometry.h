2c3c4b0b69f69e3c14bb20c7a87db18915ec04b1
Martin Ueding <dev@martin-ueding.de>
2017-04-24 20:35:42 +0200

    Add a documentation for FullCloverBlock

--- include/qphix/geometry.h
+++ include/qphix/geometry.h
@@ -95,6 +95,27 @@ namespace QPhiX {
 			T off_diag2[15][2][V];  // Complex, off diagonal part of block 2
 		};
 
+    /**
+      Clover structure suitable for twisted mass.
+
+      It has the following indices:
+
+      - The fields `block1` and `block2` refer to the half-spinors, the blocks
+      are in spin-space. Each of those, then has the following indices.
+
+      - Combined spin and color index, length 6. The color part is faster, so
+      let the combined index be `sc`. Then the (two-spinor) spin index is computed with `s2 =
+      sc / 3` and the color index with `c = sc % 3`. A four-spinor spin index
+      would be `s4 = 2 * block + s2`, where `block` is either 0 or 1, depending
+      on whether `block1` or `block2` has been chosen.
+
+      - Same as the previous index, this is a block-diagonal matrix in
+      spin-color space and therefore has a second index of that kind.
+
+      - Real and imaginary part, length 2.
+
+      - SIMD vector, iterates through the \$f x \f$ coordinate, length VECLEN.
+      */
 		struct FullCloverBlock {
 			T block1[6][6][2][V];  // Full complex, non-hermitian clover block 1
 			T block2[6][6][2][V];  // Full complex, non-hermitian clover block 2
