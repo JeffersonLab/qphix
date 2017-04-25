4971b186c01d8bd5ed40f9e8ba2a74f44ea36ef1
Martin Ueding <dev@martin-ueding.de>
2017-04-25 19:39:22 +0200

    Try the Intel arch names

--- travis-ci.sh
+++ travis-ci.sh
@@ -248,7 +248,7 @@ cxxflags="$base_cxxflags $openmp_flags $cxx11_flags $qphix_flags"
 autoreconf-if-needed
 popd
 
-for arch in 'none scalar 1' "-mavx AVX 2" "-mavx2 AVX2 2" "-mavx512 AVX512 4"; do
+for arch in 'none scalar 1' "-msandybridge AVX 2" "-mhaswell AVX2 2" "-mskylake AVX512 4"; do
     arch_a=( $arch )
     archlower=${arch_a[0]}
     archupper=${arch_a[1]}
