271236c9f77c6cdf1fe36662cf4ec5ece63e7198
Balint Joo <bjoo@jlab.org>
2017-06-11 17:44:39 -0400

    Initial stab at toplevel CMakeFile

---
+++ include/qphix/qphix_config_internal.h.in.cmake
@@ -0,0 +1,26 @@
+#ifndef QPHIX_CONFIG_H
+#define QPHIX_CONFIG_H
+
+#cmakedefine QPHIX_USE_QDPXX_ALLOC
+#cmakedefine QPHIX_USE_MM_MALLOC
+#cmakedefine QPHIX_EMIT_MESSAGES
+#cmakedefine QPHIX_SCALAR
+#cmakedefine QPHIX_PARSCALAR
+#cmakedefine QPHIX_FAKE_COMMS
+#cmakedefine QPHIX_DO_COMMS
+#cmakedefine QPHIX_USE_CEAN
+#cmakedefine QPHIX_QMP_COMMS
+#cmakedefine QPHIX_BUILD_CLOVER
+#cmakedefine QPHIX_BUILD_TWISTED_MASS
+#cmakedefine QPHIX_BUILD_TWISTED_MASS_WITH_CLOVER
+#cmakedefine QPHIX_BUILD_AVX_SOURCE
+#cmakedefine QPHIX_BUILD_AVX2_SOURCE
+#cmakedefine QPHIX_BUILD_AVX512_SOURCE
+#cmakedefine QPHIX_BUILD_SSE_SOURCE
+#cmakedefine QPHIX_BUILD_MIC_SOURCE
+#cmakedefine QPHIX_BUILD_QPX_SOURCE
+#cmakedefine QPHIX_BUILD_SCALAR_SOURCE
+#cmakedefine QPHIX_LLC_CACHE_ALIGN
+#cmakedefine QPHIX_BUILD_QDPJIT
+
+#endif
