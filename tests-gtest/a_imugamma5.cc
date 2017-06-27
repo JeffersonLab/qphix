7d034701f5a324f728ff26c1e6550b3cbf41e5b1
Martin Ueding <dev@martin-ueding.de>
2017-06-27 20:26:57 +0200

    Add two flavor test

--- tests-gtest/a_imugamma5.cc
+++ tests-gtest/a_imugamma5.cc
@@ -3,11 +3,14 @@
   routine.
   */
 
+
 #include <gtest/gtest.h>
 #include <qdp.h>
 #include <qphix/blas_new_c.h>
 
 #include "../tests/compare_qdp_spinors.h"
+
+#include <cmath>
 
 using namespace QPhiX;
 
@@ -17,11 +20,16 @@ class BlasTest : public ::testing::Test
   typedef double FT;
 
   BlasTest()
-      : latt_size_{16, 16, 16, 16}, geom_(latt_size_, 4, 4, 1, 1, 1, 1, 0, 1, true),
-        source_(geom_), step1_(geom_), step2_(geom_)
+      : latt_size_{16, 16, 16, 16}, geom_(latt_size_, 4, 4, 1, 1, 1, 1, 0, 1, false),
+        source_(geom_), source_dn_(geom_), step1_(geom_), step1_dn_(geom_),
+        step2_(geom_), step2_dn_(geom_), two_flav_source_{source_[0], source_dn_[0]},
+        two_flav_step1_{step1_[0], step1_dn_[0]},
+        two_flav_step2_{step2_[0], step2_dn_[0]}
   {
     QDP::gaussian(source_.qdp());
     source_.pack();
+    QDP::gaussian(source_dn_.qdp());
+    source_dn_.pack();
   }
 
   static int constexpr veclen = 8;
@@ -32,8 +40,15 @@ class BlasTest : public ::testing::Test
 
   Geometry<FT, veclen, soalen, compress12> geom_;
   HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> source_;
+  HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> source_dn_;
   HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> step1_;
+  HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> step1_dn_;
   HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> step2_;
+  HybridSpinor<FT, veclen, soalen, compress12, QDP::LatticeFermionD> step2_dn_;
+
+  typename Geometry<FT, veclen, soalen, compress12>::FourSpinorBlock *two_flav_source_[2];
+  typename Geometry<FT, veclen, soalen, compress12>::FourSpinorBlock *two_flav_step1_[2];
+  typename Geometry<FT, veclen, soalen, compress12>::FourSpinorBlock *two_flav_step2_[2];
 };
 
 /**
@@ -50,7 +65,7 @@ class BlasTest : public ::testing::Test
     + \frac{- \mathrm i \mu \gamma_5}{\alpha^2 + \mu^2} \,.
   \f$
   */
-TEST_F(BlasTest, apimugamma5)
+TEST_F(BlasTest, apimugamma5Inverse)
 {
   double const alpha = 3.4;
   double const mu = 1.2;
@@ -65,6 +80,45 @@ TEST_F(BlasTest, apimugamma5)
   expect_near(source_, step2_, 1e-10, geom_, 0, "α+iμγ_5 normal and inverse");
 }
 
-TEST(blas, cm)
-{
+TEST_F(BlasTest, twoFlavTwistedMassSignChange) {
+    double const alpha = 2.3;
+    double const mu = 4.2;
+    double const epsilon = 0.37;
+
+    double const apimu[2] = {alpha, mu};
+    double const apimu_minus[2] = {-alpha, -mu};
+
+    two_flav_twisted_mass(
+        apimu, epsilon, two_flav_source_, two_flav_step1_, geom_, 1);
+    two_flav_twisted_mass(
+        apimu_minus, -epsilon, two_flav_source_, two_flav_step2_, geom_, 1);
+
+    axpy<FT, veclen, soalen, compress12, 2>(
+        1.0, two_flav_step1_, two_flav_step2_, geom_, 1);
+
+    step2_.unpack();
+    step2_dn_.unpack();
+    expect_near(step1_, step2_, 1e-10, geom_, 0, "α+iμγ_5 plus and minus (up)");
+    expect_near(step2_dn_, step2_dn_, 1e-10, geom_, 0, "α+iμγ_5 plus and minus (down)");
+}
+
+TEST_F(BlasTest, twoFlavTwistedMassInverse) {
+    double const alpha = 3.0;
+    double const mu = 5.0;
+    double const epsilon = 7.0;
+    double const denominator = alpha * alpha + mu * mu - epsilon * epsilon;
+
+    double const apimu[2] = {alpha, mu};
+    double const apimu_inv[2] = {alpha / denominator, -mu / denominator};
+    double const epsilon_inv = -epsilon / denominator;
+
+    two_flav_twisted_mass(
+        apimu, epsilon, two_flav_source_, two_flav_step1_, geom_, 1);
+    two_flav_twisted_mass(
+        apimu_inv, epsilon_inv, two_flav_step1_, two_flav_step2_, geom_, 1);
+
+    step2_.unpack();
+    step2_dn_.unpack();
+    expect_near(source_, step2_, 1e-10, geom_, 0, "α+iμγ_5 normal and inverse (up)");
+    expect_near(source_dn_, step2_dn_, 1e-10, geom_, 0, "α+iμγ_5 normal and inverse (down)");
 }
